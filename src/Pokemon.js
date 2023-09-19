3434624ac203277a1edd4248ac2c0b9d811dcd2b
StevieG46 <126635670+StevieG46@users.noreply.github.com>
2023-09-19 20:41:51 +1000

    feat: add funciton pokemon name and img

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
+
 
 export default class Pokemon extends React.Component {
-    constructor() {
-        super();
+	constructor(){
+		super();
 
-        this.state = {
-            pokemoneData: {}
-        }
-    }
-    render(){
-        if (this.state.pokemoneData.name){
-            return(
-                <div>
-                    <h1>Pokemon data found!</h1>
-                </div>
-            )
-        } else {
-            return(
-                <div>
-                    <h1>Still loading pokemon data</h1>
-                </div>
-            )
+		this.state = {
+			pokemonData: {}
+		}
+	}
 
-        }
-    }
+	async componentDidMount() {
+		let randomPokemonNumber = Math.floor(Math.random() * 1010) + 1;
+		let apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber}`);
+		let apiData = await apiResponse.json();
+
+		this.setState({pokemonData: apiData});
+	}
+
+	render(){
+		if (this.state.pokemonData.name){
+			return(
+				<div>
+					<h1>{this.state.pokemonData.name}</h1>
+					<img src={this.state.pokemonData.sprites.front_default} alt="Pokemon API sprite" />
+				</div>
+			)
+		} else {
+			return(
+				<div>
+					<h1>Still loading Pokemon data...</h1>
+				</div>
+			)
+		}
+	}
 }
